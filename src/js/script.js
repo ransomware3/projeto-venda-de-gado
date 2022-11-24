f027728b6e3527fb87d3d854dc4fb0e9c18474ea
ransomware3 <natanbaldo11@gmail.com>
2022-11-24 10:51:05 -0300

    primeiro commit

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+const elementosAnimados = document.querySelectorAll('[data-anime]')
+
+function animacaoScroll(){
+    const topoDaPagina = window.pageYOffset + ((window.innerHeight * 3) / 4)
+    elementosAnimados.forEach((item) => {
+        if(topoDaPagina > item.offsetTop){
+            item.classList.add('animate')
+            console.log(item.offsetTop)
+        }else{
+            item.classList.remove('animate')
+        }
+    })
+}
+
+animacaoScroll()
+
+if(elementosAnimados.length){
+    window.addEventListener('scroll', () => {
+        animacaoScroll()
+    })
+}
